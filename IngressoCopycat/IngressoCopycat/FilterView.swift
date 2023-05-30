011c87423aa46f5f3ce981fdb022256fbada5171
Seixasr <rafaelseixas.figueiredo@gmail.com>
2023-05-30 11:48:28 -0300

    Adicionei FilterView e SearchBarView

---
+++ IngressoCopycat/IngressoCopycat/FilterView.swift
@@ -0,0 +1,20 @@
+//
+//  FilterView.swift
+//  IngressoCopycat
+//
+//  Created by Rafael Santos Seixas Figueiredo on 30/05/23.
+//
+
+import SwiftUI
+
+struct FilterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FilterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FilterView()
+    }
+}
